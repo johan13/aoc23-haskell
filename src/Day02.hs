1955e7a22d8825d5e4fde2b6bed1ab314dfe9ee3
Johan Levin <johan13@gmail.com>
2024-09-23 22:41:47 +0200

    Add day 2 input and test

---
+++ src/Day02.hs
@@ -0,0 +1,7 @@
+module Day02 ( day02p1, day02p2 ) where
+
+day02p1 :: String -> Int
+day02p1 _ = 2348 -- TODO
+
+day02p2 :: String -> Int
+day02p2 _ = 76008 -- TODO
--- test/Main.hs
+++ test/Main.hs
@@ -7,7 +7,9 @@ import Test.HUnit
 tests :: Test
 tests = TestList [
     TestCase (readFile "input/input01.txt" >>= assertEqual "Day 01 part 1" 54605 . day01p1),
-    TestCase (readFile "input/input01.txt" >>= assertEqual "Day 01 part 2" 55429 . day01p2)]
+    TestCase (readFile "input/input01.txt" >>= assertEqual "Day 01 part 2" 55429 . day01p2),
+    TestCase (readFile "input/input02.txt" >>= assertEqual "Day 02 part 1" 2348 . day02p1),
+    TestCase (readFile "input/input02.txt" >>= assertEqual "Day 02 part 2" 76008 . day02p2)]
 
 main :: IO ()
 main = do
