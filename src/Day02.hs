382eb95015ae943bb5bdb677b68d794c6d61ca00
Johan Levin <johan13@gmail.com>
2024-09-27 19:28:41 +0200

    Refactor day 2 part 1

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,11 +1,14 @@
 module Day02 (day02p1, day02p2) where
 
+import Data.List (find, isSuffixOf)
 import Data.List.Split (splitOn)
-import Data.List (find)
-import Data.Maybe (fromMaybe)
 
 day02p1 :: String -> Int
-day02p1 input = sum [getId g | g <- parseInput input, gameIsPossible g]
+day02p1 = sum . map getId . filter gameIsPossible . parseInput
+  where
+    getId (Game i _) = i
+    gameIsPossible (Game _ draws) = all drawIsPossible draws
+    drawIsPossible (Draw r g b) = r <= 12 && g <= 13 && b <= 14
 
 day02p2 :: String -> Int
 day02p2 _ = 76008 -- TODO
@@ -15,27 +18,11 @@ data Draw = Draw Int Int Int
 
 parseInput :: String -> [Game]
 parseInput = map parseGame . lines
-
-parseGame :: String -> Game
-parseGame str = Game no draws
   where
-    no = read $ takeWhile (/= ':') $ drop 5 str
-    draws = map parseDraw $ splitOn "; " $ drop 2 $ dropWhile (/= ':') str
-
-parseDraw :: String -> Draw
-parseDraw str = Draw (countColor "red" cubes) (countColor "green" cubes) (countColor "blue" cubes)
-  where cubes = splitOn ", " str
-
-countColor :: String -> [String] -> Int
-countColor color cubes =
-    let found = find (\cube -> drop (length cube - length color) cube == color) cubes
-    in read (takeWhile (/= ' ') (fromMaybe "0" found))
-
-gameIsPossible :: Game -> Bool
-gameIsPossible (Game _ draws) = all drawIsPossible draws
-
-drawIsPossible :: Draw -> Bool
-drawIsPossible (Draw r g b) = r <= 12 && g <= 13 && b <= 14
-
-getId :: Game -> Int
-getId (Game i _) = i
+    parseGame line = case splitOn ": " line of
+        [label, draws] -> Game (read $ drop 5 label) (map parseDraw $ splitOn "; " draws)
+        _              -> error "Invalid input"
+    parseDraw draw =
+        let cubes = splitOn ", " draw
+        in Draw (count "red" cubes) (count "green" cubes) (count "blue" cubes)
+    count color = maybe 0 (read . head . words) . find (color `isSuffixOf`)
