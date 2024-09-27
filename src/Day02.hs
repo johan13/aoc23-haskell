3c416e4eb6af4591d651ff10b9aef546cc629f8f
Johan Levin <johan13@gmail.com>
2024-09-27 20:11:41 +0200

    Refactor day 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -4,29 +4,26 @@ import Data.List (find, isSuffixOf)
 import Data.List.Split (splitOn)
 
 day02p1 :: String -> Int
-day02p1 = sum . map getId . filter gameIsPossible . parseInput
+day02p1 = sum . map gameId . filter gameIsPossible . parseInput
   where
-    getId (Game i _) = i
-    gameIsPossible (Game _ draws) = all drawIsPossible draws
-    drawIsPossible (Draw r g b) = r <= 12 && g <= 13 && b <= 14
+    gameIsPossible = all drawIsPossible . draws
+    drawIsPossible (r, g, b) = r <= 12 && g <= 13 && b <= 14
 
 day02p2 :: String -> Int
 day02p2 = sum . map power . parseInput
   where
-    power = product . foldr1 (zipWith max) . getAllDraws
-    getAllDraws (Game _ draws) = map drawToList draws
-    drawToList (Draw r g b) = [r, g, b]
+    power = product . minCubesRequired . draws
+    minCubesRequired = foldr1 (zipWith max) . map (\(r, g, b) -> [r, g, b])
 
-data Game = Game Int [Draw]
-data Draw = Draw Int Int Int
+data Game = Game { gameId :: Int, draws :: [(Int, Int, Int)] }
 
 parseInput :: String -> [Game]
 parseInput = map parseGame . lines
   where
     parseGame line = case splitOn ": " line of
-        [label, draws] -> Game (read $ drop 5 label) (map parseDraw $ splitOn "; " draws)
-        _              -> error "Invalid input"
+        [left, right] -> Game (read $ drop 5 left) (map parseDraw $ splitOn "; " right)
+        _             -> error "Invalid input"
     parseDraw draw =
         let cubes = splitOn ", " draw
-        in Draw (count "red" cubes) (count "green" cubes) (count "blue" cubes)
+        in (count "red" cubes, count "green" cubes, count "blue" cubes)
     count color = maybe 0 (read . head . words) . find (color `isSuffixOf`)
