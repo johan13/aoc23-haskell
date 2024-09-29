0b75e315fae36eca92b3bac66d24078c03152608
Johan Levin <johan13@gmail.com>
2024-09-29 14:39:36 +0200

    Solve day 4 part 2

--- src/Day04.hs
+++ src/Day04.hs
@@ -4,17 +4,27 @@ import Data.List (intersect)
 import Data.List.Split (splitOn)
 
 day04p1 :: String -> Int
-day04p1 = sum . map (score . uncurry intersect) . parseInput
+day04p1 = sum . map score . winnersPerCard
   where
-    score [] = 0
-    score winningNumbers = 2 ^ (length winningNumbers - 1)
+    score 0 = 0
+    score numWinners = 2 ^ (numWinners - 1)
 
 day04p2 :: String -> Int
-day04p2 _ = 11827296 -- TODO
+day04p2 = countCards (repeat 1) . winnersPerCard
+
+winnersPerCard :: String -> [Int]
+winnersPerCard = map (length . uncurry intersect) . parseInput
+
+countCards :: [Int] -> [Int] -> Int
+countCards (mult:mults) (score:scores) = mult + countCards nextMultipliers scores
+  where
+    nextMultipliers = zipWith (+) mults (replicate score mult ++ repeat 0)
+countCards _ _ = 0
 
 parseInput :: String -> [([Int], [Int])]
 parseInput = map parseLine . lines
   where
-    parseLine line = case splitOn "|" $ drop 10 line of
-        [winning, mine] -> (map read $ words winning, map read $ words mine)
+    parseLine line = case splitOn "|" $ tail $ dropWhile (/= ':') line of
+        [winning, mine] -> (parseNumbers winning, parseNumbers mine)
         _               -> error "Invalid input"
+    parseNumbers = map read . words
