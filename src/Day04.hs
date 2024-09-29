2ebf3f9a17ae56452d4d3313cfc9c57912e28547
Johan Levin <johan13@gmail.com>
2024-09-29 15:59:51 +0200

    Refactor day 4

--- src/Day04.hs
+++ src/Day04.hs
@@ -4,24 +4,25 @@ import Data.List (intersect)
 import Data.List.Split (splitOn)
 
 day04p1 :: String -> Int
-day04p1 = sum . map score . winnersPerCard
+day04p1 = sum . map (score . countWinners) . parseInput
   where
     score 0 = 0
-    score numWinners = 2 ^ (numWinners - 1)
+    score n = 2 ^ (n - 1)
 
 day04p2 :: String -> Int
-day04p2 = countCards (repeat 1) . winnersPerCard
-
-winnersPerCard :: String -> [Int]
-winnersPerCard = map (length . uncurry intersect) . parseInput
-
-countCards :: [Int] -> [Int] -> Int
-countCards (mult:mults) (score:scores) = mult + countCards nextMultipliers scores
+day04p2 = countCards (repeat 1) . map countWinners . parseInput
   where
-    nextMultipliers = zipWith (+) mults (replicate score mult ++ repeat 0)
-countCards _ _ = 0
+    countCards (mult:mults) (score:scores) = mult + countCards nextMultipliers scores
+      where
+        nextMultipliers = zipWith (+) mults (replicate score mult ++ repeat 0)
+    countCards _ _ = 0
 
-parseInput :: String -> [([Int], [Int])]
+countWinners :: Card -> Int
+countWinners = length . uncurry intersect
+
+type Card = ([Int], [Int])
+
+parseInput :: String -> [Card]
 parseInput = map parseLine . lines
   where
     parseLine line = case splitOn "|" $ tail $ dropWhile (/= ':') line of
