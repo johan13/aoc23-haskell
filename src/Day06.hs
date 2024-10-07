f13333b6db10736008b5c57d139f9d991e992ae4
Johan Levin <johan13@gmail.com>
2024-10-07 18:29:57 +0200

    Solve day 6

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,7 +1,23 @@
 module Day06 (day06p1, day06p2) where
 
 day06p1 :: String -> Int
-day06p1 = error "TODO"
+day06p1 = product . map numWaysToWin . parseInput
 
 day06p2 :: String -> Int
-day06p2 = error "TODO"
+day06p2 = numWaysToWin . fixKerning . parseInput
+  where
+    fixKerning = (\(t, d) -> (read $ concatMap show t, read $ concatMap show d)) . unzip
+
+numWaysToWin :: Game -> Int
+numWaysToWin (time, distance) = length $ filter (> distance) $ map getDistance [0..time]
+  where
+    getDistance holdTime = holdTime * (time - holdTime)
+
+type Game = (Int, Int) -- time, distance
+
+parseInput :: String -> [Game]
+parseInput input = case lines input of
+    [times, distances] -> zip (parseRow times) (parseRow distances)
+    _                  -> error "Invalid input"
+  where
+    parseRow = map read . tail . words
