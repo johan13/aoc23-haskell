de155cf7242f3945ebef3a75bf455a9f108eeb05
Johan Levin <johan13@gmail.com>
2024-10-07 23:31:27 +0200

    WIP

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,8 +1,8 @@
 module Day07 (day07p1, day07p2) where
 
-import Data.Char (isDigit)
 import Data.Function (on)
-import Data.List (group, sort, sortBy)
+import Data.List (elemIndex, group, sort, sortBy)
+import Data.Maybe (fromMaybe)
 
 day07p1 :: String -> Int
 day07p1 = sum . scores . sortBy compareHands . parseInput
@@ -10,14 +10,16 @@ day07p1 = sum . scores . sortBy compareHands . parseInput
     scores = zipWith (\rank (_, bid) -> rank * bid) [1..]
 
 day07p2 :: String -> Int
-day07p2 = error "TODO"
+day07p2 = sum . scores . sortBy compareHands' . parseInput
+  where
+    scores = zipWith (\rank (_, bid) -> rank * bid) [1..]
 
 compareHands :: Hand -> Hand -> Ordering
-compareHands (left, _) (right, _) = case (compare `on` handType) left right of
-    EQ -> compareCards left right
+compareHands (left, _) (right, _) = case (compare `on` handValue) left right of
+    EQ -> (compare `on` map cardValue) left right
     x  -> x
   where
-    handType x = case sortBy (flip compare) $ map length $ group $ sort x of
+    handValue x = case sortBy (flip compare) $ map length $ group $ sort x of
         (5:_)   -> 6 :: Int
         (4:_)   -> 5
         (3:2:_) -> 4
@@ -25,20 +27,24 @@ compareHands (left, _) (right, _) = case (compare `on` handType) left right of
         (2:2:_) -> 2
         (2:_)   -> 1
         _       -> 0
-    compareCards (l:ls) (r:rs)
-        | l == r = compareCards ls rs
-        | isDigit l && isDigit r = compare l r
-        | l == 'A' = GT
-        | r == 'A' = LT
-        | l == 'K' = GT
-        | r == 'K' = LT
-        | l == 'Q' = GT
-        | r == 'Q' = LT
-        | l == 'J' = GT
-        | r == 'J' = LT
-        | l == 'T' = GT
-        | r == 'T' = LT
-    compareCards _ _ = error "Wat?"
+    cardValue c = fromMaybe (error "Invalid card") (elemIndex c "23456789TJQKA")
+
+compareHands' :: Hand -> Hand -> Ordering
+compareHands' (left, _) (right, _) = case (compare `on` handValue) left right of
+    EQ -> (compare `on` map cardValue) left right
+    x  -> x
+  where
+    handValue x = case foo x of
+        (5:_)   -> 6 :: Int
+        (4:_)   -> 5
+        (3:2:_) -> 4
+        (3:_)   -> 3
+        (2:2:_) -> 2
+        (2:_)   -> 1
+        _       -> 0
+    cardValue c = fromMaybe (error "Invalid card") (elemIndex c "J23456789TQKA")
+    foo :: [Char] -> [Int] -- TODO: Handle J as wildcard
+    foo x = sortBy (flip compare) $ map length $ group $ sort x
 
 type Hand = ([Char], Int) -- cards, bid
 
