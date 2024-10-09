46f4dc15796e15126a2af99e3260d8c5d7d43c59
Johan Levin <johan13@gmail.com>
2024-10-09 16:57:21 +0200

    WIP

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,7 +1,18 @@
 module Day08 (day08p1, day08p2) where
 
+import Data.Map.Strict (Map, fromList)
+
 day08p1 :: String -> Int
-day08p1 = error "TODO"
+day08p1 = length . snd . parseInput
 
 day08p2 :: String -> Int
 day08p2 = error "TODO"
+
+type Network = Map String (String, String)
+
+parseInput :: String -> ([Char], Network)
+parseInput input = case lines input of
+    (directions:"":nodes) -> (directions, fromList $ map parseNode nodes)
+    _                     -> error "Invalid input"
+  where
+    parseNode s = (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))
