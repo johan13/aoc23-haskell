10db05ed6bfb9dd1209b91170f7c358151a4aadd
Johan Levin <johan13@gmail.com>
2024-10-14 12:30:50 +0200

    Solve day 8 part 2

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,6 +1,7 @@
 module Day08 (day08p1, day08p2) where
 
-import Data.Map.Strict (Map, fromList, (!))
+import Data.Map.Strict (Map, fromList, (!), keys)
+import Data.List (isSuffixOf)
 
 day08p1 :: String -> Int
 day08p1 = follow "AAA" . parseInput
@@ -12,7 +13,19 @@ day08p1 = follow "AAA" . parseInput
         in 1 + follow next (instr, network)
 
 day08p2 :: String -> Int
-day08p2 = error "TODO"
+day08p2 = foldl1 lcm . findCycles . parseInput
+  where
+    findCycles :: ([Char], Network) -> [Int]
+    findCycles (directions, network) =
+        let starts = filter ("A" `isSuffixOf`) (keys network)
+            cycles = map (findCycle directions) starts
+        in cycles
+      where
+        findCycle :: String -> String -> Int
+        findCycle [] _ = 0
+        findCycle (d:ds) pos
+            | "Z" `isSuffixOf` pos = 0
+            | otherwise = 1 + findCycle ds ((if d == 'L' then fst else snd) $ network ! pos)
 
 type Network = Map String (String, String)
 
