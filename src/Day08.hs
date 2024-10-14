f1d987e682cf4733bc266f2dd10cc95cbff91f25
Johan Levin <johan13@gmail.com>
2024-10-14 12:15:04 +0200

    Solve day 8 part 1

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,9 +1,15 @@
 module Day08 (day08p1, day08p2) where
 
-import Data.Map.Strict (Map, fromList)
+import Data.Map.Strict (Map, fromList, (!))
 
 day08p1 :: String -> Int
-day08p1 = length . snd . parseInput
+day08p1 = follow "AAA" . parseInput
+  where
+    follow _ ([], _) = 0
+    follow "ZZZ" _ = 0
+    follow current (i:instr, network) =
+        let next = (if i == 'L' then fst else snd) $ network ! current
+        in 1 + follow next (instr, network)
 
 day08p2 :: String -> Int
 day08p2 = error "TODO"
@@ -12,7 +18,7 @@ type Network = Map String (String, String)
 
 parseInput :: String -> ([Char], Network)
 parseInput input = case lines input of
-    (directions:"":nodes) -> (directions, fromList $ map parseNode nodes)
+    (directions:"":nodes) -> (cycle directions, fromList $ map parseNode nodes)
     _                     -> error "Invalid input"
   where
     parseNode s = (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))
