d6f2fac601fdce48ad074b77d9e1cffa6bea507f
Johan Levin <johan13@gmail.com>
2024-10-14 21:36:43 +0200

    Refactor day 10 part 1

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,25 +1,25 @@
 module Day10 (day10p1, day10p2) where
 
 day10p1 :: String -> Int
-day10p1 = farthestPoint . lines
-  where
-    -- S is at 53,75 and it covers a '-'. Start at 54,75 going right and add 1 to the distance.
-    farthestPoint grid = (1 + measureLoop grid (54, 75) 'R') `div` 2
-    measureLoop grid (x, y) heading = 1 + case grid !! y !! x of
-        'S' -> 0
-        'J' | heading == 'R' -> measureLoop grid (x, y - 1) 'U'
-        '-' | heading == 'R' -> measureLoop grid (x + 1, y) 'R'
-        '7' | heading == 'R' -> measureLoop grid (x, y + 1) 'D'
-        'L' | heading == 'L' -> measureLoop grid (x, y - 1) 'U'
-        '-' | heading == 'L' -> measureLoop grid (x - 1, y) 'L'
-        'F' | heading == 'L' -> measureLoop grid (x, y + 1) 'D'
-        '7' | heading == 'U' -> measureLoop grid (x - 1, y) 'L'
-        '|' | heading == 'U' -> measureLoop grid (x, y - 1) 'U'
-        'F' | heading == 'U' -> measureLoop grid (x + 1, y) 'R'
-        'J' | heading == 'D' -> measureLoop grid (x - 1, y) 'L'
-        '|' | heading == 'D' -> measureLoop grid (x, y + 1) 'D'
-        'L' | heading == 'D' -> measureLoop grid (x + 1, y) 'R'
-        _ -> error "Invalid map"
+day10p1 input =
+    let pipe = tracePipe (lines input) (53, 75) 'R' -- 'S' is at 53,75 and it covers a '-'.
+    in length pipe `div` 2
 
 day10p2 :: String -> Int
 day10p2 = error "TODO"
+
+tracePipe :: [[Char]] -> (Int, Int) -> Char -> [(Int, Int)]
+tracePipe grid pos@(x,y) heading = pos :
+    let x' = x + case heading of 'R' -> 1; 'L' -> -1; _ -> 0
+        y' = y + case heading of 'D' -> 1; 'U' -> -1; _ -> 0
+    in case (heading, grid !! y' !! x') of
+        (_  , 'S') -> []
+        ('R', 'J') -> tracePipe grid (x', y') 'U'
+        ('R', '7') -> tracePipe grid (x', y') 'D'
+        ('L', 'L') -> tracePipe grid (x', y') 'U'
+        ('L', 'F') -> tracePipe grid (x', y') 'D'
+        ('U', '7') -> tracePipe grid (x', y') 'L'
+        ('U', 'F') -> tracePipe grid (x', y') 'R'
+        ('D', 'J') -> tracePipe grid (x', y') 'L'
+        ('D', 'L') -> tracePipe grid (x', y') 'R'
+        _          -> tracePipe grid (x', y') heading
