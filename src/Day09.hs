fd7a0e2ee20aee6881d25f11121e0ac655693a85
Johan Levin <johan13@gmail.com>
2024-10-14 17:26:30 +0200

    Solve day 9

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,7 +1,15 @@
 module Day09 (day09p1, day09p2) where
 
 day09p1 :: String -> Int
-day09p1 = error "TODO"
+day09p1 = sum . map (nextInSequence . map read . words) . lines
+  where
+    nextInSequence x
+        | all (== 0) x = 0
+        | otherwise = last x + nextInSequence (zipWith (-) (tail x) x)
 
 day09p2 :: String -> Int
-day09p2 = error "TODO"
+day09p2 = sum . map (prevInSequence . map read . words) . lines
+  where
+    prevInSequence x
+        | all (== 0) x = 0
+        | otherwise = head x - prevInSequence (zipWith (-) (tail x) x)
