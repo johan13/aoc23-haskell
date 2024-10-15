ab0a5e7187b76eb9366aab1d089a9919a0876123
Johan Levin <johan13@gmail.com>
2024-10-15 08:42:58 +0200

    Refactor day 9

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,14 +2,12 @@ module Day09 (day09p1, day09p2) where
 
 day09p1 :: String -> Int
 day09p1 = sum . map (nextInSequence . map read . words) . lines
-  where
-    nextInSequence x
-        | all (== 0) x = 0
-        | otherwise = last x + nextInSequence (zipWith (-) (tail x) x)
 
 day09p2 :: String -> Int
-day09p2 = sum . map (prevInSequence . map read . words) . lines
-  where
-    prevInSequence x
-        | all (== 0) x = 0
-        | otherwise = head x - prevInSequence (zipWith (-) (tail x) x)
+day09p2 = sum . map (nextInSequence . reverse . map read . words) . lines
+
+nextInSequence :: [Int] -> Int
+nextInSequence x
+    | all (== 0) x = 0
+    | otherwise = let differences = zipWith (-) (tail x) x
+                  in last x + nextInSequence differences
