9e36f5178c4d242a5bdafdf8f8a372bf53bfc540
Johan Levin <johan13@gmail.com>
2024-10-24 19:17:23 +0200

    Solve day 11 part 2

--- src/Day11.hs
+++ src/Day11.hs
@@ -4,21 +4,21 @@ module Day11 (day11p1, day11p2) where
 import Data.List (elemIndices, nub)
 
 day11p1 :: String -> Int
-day11p1 = sum . map (uncurry distance) . makePairs . expand . parseInput
+day11p1 = sum . map (uncurry distance) . makePairs . expand 2 . parseInput
 
 day11p2 :: String -> Int
-day11p2 = error "TODO"
+day11p2 = sum . map (uncurry distance) . makePairs . expand 1000000 . parseInput
 
 parseInput :: String -> [(Int, Int)]
 parseInput = concat . zipWith (\y -> map (, y) . elemIndices '#') [0..] . lines
 
-expand :: [(Int, Int)] -> [(Int, Int)]
-expand galaxies =
+expand :: Int -> [(Int, Int)] -> [(Int, Int)]
+expand factor galaxies =
     let xs = nub $ map fst galaxies
         ys = nub $ map snd galaxies
         emptyCols = [x | x <- [0..maximum xs], x `notElem` xs]
         emptyRows = [y | y <- [0..maximum ys], y `notElem` ys]
-        expand' (x, y) = (x + length (filter (< x) emptyCols), y + length (filter (< y) emptyRows))
+        expand' (x, y) = (x + (factor - 1) * length (filter (< x) emptyCols), y + (factor - 1) * length (filter (< y) emptyRows))
     in map expand' galaxies
 
 makePairs :: [a] -> [(a, a)]
