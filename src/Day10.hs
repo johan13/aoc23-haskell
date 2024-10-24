d558507fe3047fdf544f756d736ba217c09e8638
Johan Levin <johan13@gmail.com>
2024-10-24 18:02:19 +0200

    Refactor day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,24 +1,23 @@
 module Day10 (day10p1, day10p2) where
 
+import Data.Set (Set, empty, insert, member, size)
+
 day10p1 :: String -> Int
 day10p1 input =
     let pipe = tracePipe (lines input) (53, 75) 'R' -- 'S' is at 53,75 and it covers a '-'.
-    in length pipe `div` 2
+    in size pipe `div` 2
 
 day10p2 :: String -> Int
 day10p2 input =
     let grid = lines input
-        pipe = tracePipe grid (53, 75) 'R'
-        grid' = cleanGrid pipe grid
-        posTypes = map (scanl scanner Outside) grid'
-    in sum $ map (length . filter (== Inside)) posTypes
+    in sum $ map countInside $ cleanUpGrid grid $ tracePipe grid (53, 75) 'R'
 
-tracePipe :: [[Char]] -> (Int, Int) -> Char -> [(Int, Int)]
-tracePipe grid pos@(x,y) heading = pos :
+tracePipe :: [[Char]] -> (Int, Int) -> Char -> Set (Int, Int)
+tracePipe grid pos@(x, y) heading = insert pos $
     let x' = x + case heading of 'R' -> 1; 'L' -> -1; _ -> 0
         y' = y + case heading of 'D' -> 1; 'U' -> -1; _ -> 0
     in case (heading, grid !! y' !! x') of
-        (_  , 'S') -> []
+        (_  , 'S') -> empty
         ('R', 'J') -> tracePipe grid (x', y') 'U'
         ('R', '7') -> tracePipe grid (x', y') 'D'
         ('L', 'L') -> tracePipe grid (x', y') 'U'
@@ -29,24 +28,25 @@ tracePipe grid pos@(x,y) heading = pos :
         ('D', 'L') -> tracePipe grid (x', y') 'R'
         _          -> tracePipe grid (x', y') heading
 
--- "pipe -> grid -> cleanGrid" Replace disconnected pipe segments with '.'
-cleanGrid :: [(Int, Int)] -> [[Char]] -> [[Char]]
-cleanGrid pipe = mapWithPos (\pos c -> if pos `elem` pipe then c else '.')
+cleanUpGrid :: [[Char]] -> Set (Int, Int) -> [[Char]]
+cleanUpGrid grid pipe = mapWithPos (\pos ch -> if pos `member` pipe then ch else '.') grid
   where
     mapWithPos fn = zipWith (\y -> zipWith (\x c -> fn (x, y) c) [0..]) [0..]
 
-data PosType = Outside | JustInside | Inside | BorderInsideUp | BorderInsideDown deriving (Eq)
+data ScanState = Outside | Inside | BorderInsideUp | BorderInsideDown deriving (Eq)
 
-scanner :: PosType -> Char -> PosType
-scanner Outside        '|' = JustInside
-scanner _              '|' = Outside
-scanner Outside        'L' = BorderInsideUp
-scanner _              'L' = BorderInsideDown
-scanner Outside        'F' = BorderInsideDown
-scanner _              'F' = BorderInsideUp
-scanner BorderInsideUp 'J' = Outside
-scanner _              'J' = JustInside
-scanner BorderInsideUp '7' = JustInside
-scanner _              '7' = Outside
-scanner JustInside     '.' = Inside
-scanner p _ = p
+countInside :: [Char] -> Int
+countInside = fst . foldl foldOp (0, Outside)
+  where
+    foldOp (n, state) ch = (if state == Inside && ch == '.' then n + 1 else n, nextState ch state)
+    nextState '|' Outside          = Inside
+    nextState '|' Inside           = Outside
+    nextState 'L' Outside          = BorderInsideUp
+    nextState 'L' Inside           = BorderInsideDown
+    nextState 'F' Outside          = BorderInsideDown
+    nextState 'F' Inside           = BorderInsideUp
+    nextState 'J' BorderInsideUp   = Outside
+    nextState 'J' BorderInsideDown = Inside
+    nextState '7' BorderInsideUp   = Inside
+    nextState '7' BorderInsideDown = Outside
+    nextState _ p = p
