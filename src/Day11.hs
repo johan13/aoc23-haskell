b204e0a707f82fd55eb419a2c4d0fd10b3ce28f5
Johan Levin <johan13@gmail.com>
2024-10-24 21:09:46 +0200

    Refactor day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,30 +1,31 @@
 {-# LANGUAGE TupleSections #-}
 module Day11 (day11p1, day11p2) where
 
-import Data.List (elemIndices, nub)
+import Data.List (elemIndices)
 
 day11p1 :: String -> Int
-day11p1 = sum . map (uncurry distance) . makePairs . expand 2 . parseInput
+day11p1 = sum . map (uncurry distance) . pairs . expand 2 . parseInput
 
 day11p2 :: String -> Int
-day11p2 = sum . map (uncurry distance) . makePairs . expand 1000000 . parseInput
+day11p2 = sum . map (uncurry distance) . pairs . expand 1000000 . parseInput
 
-parseInput :: String -> [(Int, Int)]
+type Galaxy = (Int, Int)
+
+parseInput :: String -> [Galaxy]
 parseInput = concat . zipWith (\y -> map (, y) . elemIndices '#') [0..] . lines
 
-expand :: Int -> [(Int, Int)] -> [(Int, Int)]
-expand factor galaxies =
-    let xs = nub $ map fst galaxies
-        ys = nub $ map snd galaxies
-        emptyCols = [x | x <- [0..maximum xs], x `notElem` xs]
-        emptyRows = [y | y <- [0..maximum ys], y `notElem` ys]
-        expand' (x, y) = (x + (factor - 1) * length (filter (< x) emptyCols), y + (factor - 1) * length (filter (< y) emptyRows))
-    in map expand' galaxies
+expand :: Int -> [Galaxy] -> [Galaxy]
+expand factor galaxies = map expand' galaxies
+  where
+    emptyX = [x | x <- [0..], x `notElem` map fst galaxies]
+    emptyY = [y | y <- [0..], y `notElem` map snd galaxies]
+    expand' (x, y) = (
+        x + (factor - 1) * length (takeWhile (< x) emptyX),
+        y + (factor - 1) * length (takeWhile (< y) emptyY))
 
-makePairs :: [a] -> [(a, a)]
-makePairs [] = []
-makePairs [_] = []
-makePairs (x:xs) = map (x,) xs ++ makePairs xs
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (x:xs) = map (x,) xs ++ pairs xs
 
-distance :: Num a => (a, a) -> (a, a) -> a
+distance :: Galaxy -> Galaxy -> Int
 distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
